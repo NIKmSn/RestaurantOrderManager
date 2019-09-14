0fb7204ceec9f77d73388936a6f32599eb65a930
NIK <nekit.gorak@gmail.com>
2019-09-14 21:27:11 +0300

    Added Item class

---
+++ RestaurantOrderManager/RestaurantOrderManager/Item.h
@@ -0,0 +1,89 @@
+#pragma once
+using namespace std;
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+using namespace std;
+using namespace System::Data::Sql;
+using namespace System::Data::SqlClient;
+using namespace System::Data::SqlTypes;
+using namespace Microsoft::SqlServer::Server;
+using namespace System::Runtime::InteropServices;
+
+ref class Item
+{
+private:
+	//Поля
+	int id;
+	String^ item;
+	int customerId;
+	int tableId;
+	DateTime^ date;
+protected:
+	//Методы заполнения полей
+	void SetItem(String^ value)
+	{
+		item = value;
+	}
+
+	void SetCustomerId(int value)
+	{
+		customerId = value;
+	}
+
+	void SetTableId(int value)
+	{
+		tableId = value;
+	}
+
+	void SetDate(DateTime^ value)
+	{
+		date = value;
+	}
+public:
+	//Методы чтения полей
+	int GetId()
+	{
+		return id;
+	}
+
+	String^ GetItem()
+	{
+		return item;
+	}
+
+	int GetCustomerId()
+	{
+		return customerId;
+	}
+
+	int GetTableId()
+	{
+		return tableId;
+	}
+
+	DateTime^ GetDate()
+	{
+		return date;
+	}
+
+	//Конструкторы
+	Item()
+	{
+		SetItem("");
+		SetCustomerId(-1);
+		SetTableId(-1);
+		SetDate(gcnew DateTime);
+	}
+	Item(String^ item, int customerId, int tableId, DateTime^ date)
+	{
+		SetItem(item);
+		SetCustomerId(customerId);
+		SetTableId(tableId);
+		SetDate(date);
+	}
+};
+
