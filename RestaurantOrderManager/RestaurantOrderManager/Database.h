6a610dc09a5a2bd84c44350289efbecb7522cf9e
NIK <nekit.gorak@gmail.com>
2019-09-17 22:28:58 +0300

    Completed database class

--- RestaurantOrderManager/RestaurantOrderManager/Database.h
+++ RestaurantOrderManager/RestaurantOrderManager/Database.h
@@ -15,31 +15,79 @@ using namespace System::Collections::Generic;
 ref class Database
 {
 
-private: String^ connStr = L"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Programs\\repos\\RestaurantOrderManager\\RestaurantOrderManager\\RestaurantDatabase\\Restaurant.mdf;Integrated Security=True;Connect Timeout=30";
-	SqlConnection^ myConnection;
-	SqlCommand^ sqlCommand = gcnew SqlCommand();
-	SqlDataAdapter^ sda = gcnew SqlDataAdapter();
-	SqlDataReader^ sqlReader;
-	
-public: SqlConnection^ GetConnection(SqlCommand^& sqlCommand)
+private: static String^ connStr = L"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Programs\\repos\\RestaurantOrderManager\\RestaurantOrderManager\\RestaurantDatabase\\Restaurant.mdf;Integrated Security=True;Connect Timeout=30";
+public: static SqlConnection^ CreateConnection()
+{
+	try
 	{
-		try
-		{
-			myConnection = gcnew SqlConnection(connStr);
-			sqlCommand->Connection = myConnection;
-		}
-		catch (Exception^ ex)
-		{
-			int id;
-			MessageBox::Show(ex->Message, "Îøèáêà", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		}
+		SqlConnection^ myConnection = gcnew SqlConnection(connStr);
+		return myConnection;
 	}
-	myConnection = gcnew SqlConnection(connStr);
-	sqlCommand->Connection = myConnection;
-	sqlCommand->Parameters->AddWithValue("@product_type", "");
-	sqlCommand->Parameters->AddWithValue("@product_name", "");
-	sqlCommand->Parameters->AddWithValue("@item_invoiceId", "");
-	sqlCommand->Parameters->AddWithValue("@item_productId", "");
-	sqlCommand->Parameters->AddWithValue("@item_quantity", "");
+	catch (Exception^ ex)
+	{
+		MessageBox::Show(ex->Message, "Îøèáêà", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+}
+public: static SqlCommand^ CreateCommand()
+{
+	return gcnew SqlCommand();
+}
+public: static SqlConnection^ CreateOpenConnection()
+{
+	try
+	{
+		SqlConnection^ connection = CreateConnection();
+		connection->Open();
+		return connection;
+	}
+	catch (Exception^ ex)
+	{
+		MessageBox::Show(ex->Message, "Îøèáêà", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+}
+public: static SqlCommand^ CreateCommand(String^ commandText, SqlConnection^ connection)
+{
+	SqlCommand^ command = CreateCommand();
+	command->CommandText = commandText;
+	command->Connection = connection;
+	command->CommandType = CommandType::Text;
+	return command;
+}
+public: static SqlCommand^ CreateStoredProcedureCommand(String^ procName, SqlConnection^ connection)
+{
+	SqlCommand^ command = CreateCommand();
+	command->CommandText = procName;
+	command->Connection = connection;
+	command->CommandType = CommandType::StoredProcedure;
+	return command;
+}
+public: static SqlDataAdapter^ CreateDataAdapter()
+{
+	return gcnew SqlDataAdapter();
+}
+public: static SqlDataReader^ CreateSqlReader()
+{
+	return gcnew SqlDataReader();
+}
+public: static SqlParameter^ CreateParameter(String^ parameterName, Object^ parameterValue)
+{
+	return gcnew SqlParameter(parameterName, parameterValue);
+}
+
+public: static Database database;
+public: static SqlDataReader^ sqlReader = CreateSqlReader();
+//public: SqlCommand^ GetProduct()
+//{
+//	SqlCommand^ sqlCommand = gcnew SqlCommand();
+//	sqlCommand->Connection = myConnection;
+//	sqlCommand.
+//}
+	//myConnection = GetConnection();
+	//sqlCommand->Connection = myConnection;
+	//sqlCommand->Parameters->AddWithValue("@product_type", "");
+	//sqlCommand->Parameters->AddWithValue("@product_name", "");
+	//sqlCommand->Parameters->AddWithValue("@item_invoiceId", "");
+	//sqlCommand->Parameters->AddWithValue("@item_productId", "");
+	//sqlCommand->Parameters->AddWithValue("@item_quantity", "");
 };
 
