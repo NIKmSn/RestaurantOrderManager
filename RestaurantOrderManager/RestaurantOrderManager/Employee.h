0c1565f276bf679d8c5186a932635cda912f7518
NIK <nekit.gorak@gmail.com>
2019-09-22 23:11:41 +0300

    Refactoring Employee class

--- RestaurantOrderManager/RestaurantOrderManager/Employee.h
+++ RestaurantOrderManager/RestaurantOrderManager/Employee.h
@@ -9,8 +9,20 @@ ref class Employee :
 		String^ title;
 		Decimal salary;
 		String^ photo;
+private: void SetId(int value)
+{
+	id = value;
+}
 	protected:
 		//Методы заполнения полей класса
+		void SetId()
+		{
+			SqlConnection^ conn = Database::CreateConnection();
+			SqlCommand^ sqlCommand = Database::CreateCommand("SELECT MAX(Id) FROM Employee", conn);
+			conn->Open();
+			id = (int)sqlCommand->ExecuteScalar();
+			conn->Close();
+		}
 		void SetTitle(String^ value)
 		{
 			title = value;
@@ -56,6 +68,26 @@ ref class Employee :
 			SetSalary(salary);
 			SetPhoto(photo);
 		}
+public: static Employee^ GetEmployee(int id)
+{
+	SqlConnection^ conn = Database::CreateOpenConnection();
+	SqlCommand^ sqlCommand = Database::CreateStoredProcedureCommand("SELECT * FROM Employee WHERE Id = @id", conn);
+	sqlCommand->Parameters->Add("@id", SqlDbType::Int)->Value = 1;
+	SqlDataReader^ sqlReader = sqlCommand->ExecuteReader();
+	conn->Close();
 
+	if (sqlReader->Read())
+	{
+		Employee^ employee = gcnew Employee();
+		employee->SetId(id);
+		employee->SetName(sqlReader["Name"]->ToString());
+		employee->SetPhoneNumber(sqlReader["PhoneNumber"]->ToString());
+		employee->SetTitle(sqlReader["Title"]->ToString());
+		employee->SetSalary(Convert::ToDecimal(sqlReader["Salary"]));
+		employee->SetPhoto(sqlReader["Photo"]->ToString());
+		return employee;
+	}
+	return nullptr;
+}
 };
 
